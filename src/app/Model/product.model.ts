cc6bc490d45adf964079229d53d7fd590a2c83d1
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-30 17:17:07 +0600

    brought changes to api to send specific data for adminPanel for product. refactored the frontend accordingly

--- src/app/Model/product.model.ts
+++ src/app/Model/product.model.ts
@@ -1,13 +1,12 @@
-import { StockModel } from "./stock.model";
-
 export interface ProductModel {
   productId: number;
   name: string; 
   brand: string;
   description: string;
-  isDiscounted: boolean;
-  category: any;
-  stocks: StockModel;
+  isDiscounted: boolean; 
+  sellingPrice: number;
+  quantity: number;
+  categoryName: string;
   imageUrl: string; // URL to the product image 
   createdBy: string; 
   createdDate: Date;
