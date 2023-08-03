2482ae3c2c3117330ad52173f255a8840ad2f2bd
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-08-04 03:42:41 +0600

    added primeNg pagination comp, added customer event handleUserClick to send data to its parent component

--- src/app/components/shared/page-navigation/page-navigation.component.ts
+++ src/app/components/shared/page-navigation/page-navigation.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-page-navigation',
   templateUrl: './page-navigation.component.html',
-  styleUrls: ['./page-navigation.component.css']
+  styleUrls: ['./page-navigation.component.css'],
 })
 export class PageNavigationComponent {
+  @Output() handleUserClick = new EventEmitter();
+  pageNumber!: number;
+  @Input() totalRecords: number = 20;
+  first: number = 0;
+  rows: number = 20;
 
+  onPageChange(event: any) {
+    this.first = event.first;
+    this.rows = event.rows;
+    this.pageNumber = event.page + 1;
+    this.handleUserClick.emit(this.pageNumber);
+  }
 }
