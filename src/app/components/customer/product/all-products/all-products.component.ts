8e4df5e8bf1036c7493e83edf0452b7b6f561bb6
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-08-03 16:40:40 +0600

    shows product card from typescript

--- src/app/components/customer/product/all-products/all-products.component.ts
+++ src/app/components/customer/product/all-products/all-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomerProductModel } from 'src/app/Model/customer-product.model';
 
 @Component({
   selector: 'app-all-products',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-products.component.css']
 })
 export class AllProductsComponent {
+  products: CustomerProductModel[] = [
+    {
+      imageUrl: 'assets/products/aci-pure-salt-1-kg.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
+    {
+      imageUrl: 'assets/products/roshun-garlic-imported-25-gm-500-gm.jpg',
+      name: 'Beauty Makeup',
+      weight: '500 gm',
+      sellingPrice: 150,
+    },{
+      imageUrl: 'assets/products/lizol-floor-cleaner-floral-disinfectant-surface-1-ltr.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
+    {
+      imageUrl: 'assets/products/rupchanda-fortified-soyabean-oil-5-ltr.jpg',
+      name: 'Beauty Makeup',
+      weight: '500 gm',
+      sellingPrice: 150,
+    },
+    {
+      imageUrl: 'assets/products/potato-regular-50-gm-1-kg.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
+    {
+      imageUrl: 'assets/products/deshi-gajor-local-carrot-25-gm-500-gm.jpg',
+      name: 'Beauty Makeup',
+      weight: '500 gm',
+      sellingPrice: 150,
+    },
+    // Add more products as needed
+  ];
+  slicedPproducts: any[];
+
+    constructor() {
+      this.slicedPproducts = this.products.slice(0, 6);
+     }
 
 }
