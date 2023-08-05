224690fcfb6cbd67827575e024367beaa8111b8d
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-08-05 11:24:12 +0600

    side nav for cart

--- src/app/components/customer/side-nav/side-nav.component.ts
+++ src/app/components/customer/side-nav/side-nav.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
 })
 export class SideNavComponent {
-
+  sidebarVisible!: boolean;
 }
