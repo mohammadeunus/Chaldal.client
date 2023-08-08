febbd50c2218a12d4e36f9273a144464c90c8323
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-08-09 03:38:43 +0600

    added method for button for items in cart

--- src/app/components/customer/cart/cart.component.ts
+++ src/app/components/customer/cart/cart.component.ts
@@ -5,28 +5,34 @@ import { SharedService } from 'src/app/Services/shared/shared.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  removeFromCart(_t9: CartModel) {
-    throw new Error('Method not implemented.');
-  }
   cartItems: CartModel[] = []; // Initialize an empty cart
   numberOfCartItems: number = this.cartItems.length;
   TotalPrice: number = 0;
 
-  constructor(private sharedServices: SharedService) { }
+  constructor(private sharedServices: SharedService) {}
 
   ngOnInit() {
     this.getDataFromProductCard();
   }
 
   getDataFromProductCard() {
-    //on add to cart button click from product card 
+    //on add to cart button click from product card
     this.sharedServices.getData().subscribe((data) => {
-      this.cartItems.push({ quantity: 0, productRefId: data.ProductId, productName: data.name, productSellingPrice: data.sellingPrice, customerRefId: 21 });
+      this.cartItems.push({
+        quantity: 0,
+        productRefId: data.ProductId,
+        productName: data.name,
+        productSellingPrice: data.sellingPrice,
+        customerRefId: 21,
+      });
       this.numberOfCartItems = this.cartItems.length;
-      this.TotalPrice = this.cartItems.reduce((acc, item) => acc + item.productSellingPrice, 0);
+      this.TotalPrice = this.cartItems.reduce(
+        (acc, item) => acc + item.productSellingPrice,
+        0
+      );
     });
   }
 
@@ -39,5 +45,14 @@ export class CartComponent implements OnInit {
   checkout() {
     console.log('Checkout', this.cartItems);
     this.emptyCart();
+  }
+  removeFromCart(_t9: CartModel) {
+    throw new Error('Method not implemented.');
+  }
+  increamentQnty() {
+    throw new Error('Method not implemented.');
+  }
+  decreamentQnty() {
+    throw new Error('Method not implemented.');
   }
 }
