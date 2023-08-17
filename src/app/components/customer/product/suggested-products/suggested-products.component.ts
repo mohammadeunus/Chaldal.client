5b105d163e8a59666bdcc1f6e57ed049a553174e
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-08-18 05:44:52 +0600

    updated suggested products

--- src/app/components/customer/product/suggested-products/suggested-products.component.ts
+++ src/app/components/customer/product/suggested-products/suggested-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
 import { CustomerProductModel } from 'src/app/Model/customer-product.model';
 import { ProductModel } from 'src/app/Model/product.model';
 import { ProductService } from 'src/app/Services/product/product.service';
@@ -6,8 +6,8 @@ import { ProductService } from 'src/app/Services/product/product.service';
 @Component({
   selector: 'app-suggested-products',
   templateUrl: './suggested-products.component.html',
-  styleUrls: ['./suggested-products.component.css']
-}) 
+  styleUrls: ['./suggested-products.component.css'],
+})
 export class SuggestedProductsComponent {
   products: CustomerProductModel[] = [
     {
@@ -23,9 +23,33 @@ export class SuggestedProductsComponent {
       name: 'Beauty Makeup',
       weight: '500 gm',
       sellingPrice: 150,
-    },{
+    },
+    {
       ProductId: 1,
-      imageUrl: 'assets/products/lizol-floor-cleaner-floral-disinfectant-surface-1-ltr.jpg',
+      imageUrl:
+        'assets/products/lizol-floor-cleaner-floral-disinfectant-surface-1-ltr.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
+    {
+      ProductId: 1,
+      imageUrl: 'assets/products/aci-pure-salt-1-kg.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
+    {
+      ProductId: 1,
+      imageUrl: 'assets/products/roshun-garlic-imported-25-gm-500-gm.jpg',
+      name: 'Beauty Makeup',
+      weight: '500 gm',
+      sellingPrice: 150,
+    },
+    {
+      ProductId: 1,
+      imageUrl:
+        'assets/products/lizol-floor-cleaner-floral-disinfectant-surface-1-ltr.jpg',
       name: 'Aci Pure Salt 1 kg',
       weight: '1 kg',
       sellingPrice: 100,
@@ -51,11 +75,36 @@ export class SuggestedProductsComponent {
       weight: '500 gm',
       sellingPrice: 150,
     },
+    {
+      ProductId: 1,
+      imageUrl: 'assets/products/aci-pure-salt-1-kg.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
+    {
+      ProductId: 1,
+      imageUrl: 'assets/products/roshun-garlic-imported-25-gm-500-gm.jpg',
+      name: 'Beauty Makeup',
+      weight: '500 gm',
+      sellingPrice: 150,
+    },
+    {
+      ProductId: 1,
+      imageUrl:
+        'assets/products/lizol-floor-cleaner-floral-disinfectant-surface-1-ltr.jpg',
+      name: 'Aci Pure Salt 1 kg',
+      weight: '1 kg',
+      sellingPrice: 100,
+    },
     // Add more products as needed
   ];
 
   // Function to organize products into groups of three
-  groupProducts(products: CustomerProductModel[], groupSize: number): CustomerProductModel[][] {
+  groupProducts(
+    products: CustomerProductModel[],
+    groupSize: number
+  ): CustomerProductModel[][] {
     const groupedProducts: CustomerProductModel[][] = [];
     for (let i = 0; i < products.length; i += groupSize) {
       groupedProducts.push(products.slice(i, i + groupSize));
@@ -64,5 +113,8 @@ export class SuggestedProductsComponent {
   }
 
   // Group products into sets of three
-  groupedProducts: CustomerProductModel[][] = this.groupProducts(this.products, 3);
+  groupedProducts: CustomerProductModel[][] = this.groupProducts(
+    this.products,
+    3
+  );
 }
